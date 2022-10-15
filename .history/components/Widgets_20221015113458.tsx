d85abff926fcf2410b5f4a0fe5a70d6c9d061733
Matthew Chan <mattchan1999@gmail.com>
2022-10-15 11:36:24 -0400

    added margin top to widget

--- .history/components/Widgets_20221015113458.tsx
+++ .history/components/Widgets_20221015113458.tsx
@@ -6,13 +6,14 @@ function Widgets() {
   return (
     <div className='px-2 mt-2 col-span-2 hidden lg:inline'>
       {/* Search box */}
-      <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2 mb-2'>
+      <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2 '>
          <SearchIcon className='h-5 w-5 text-gray-400'/>
          <input type="text" placeholder='Search Twitter' className='flex-1 bg-transparent outline-none'/>
       </div>
       <TwitterTimelineEmbed
          sourceType="profile"
          screenName="elonmusk"
+         className='mt-2'
          options={{height: 1000}}/>
     </div>
   )
